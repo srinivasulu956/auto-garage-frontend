4e47ee4f42ddeb36accc03b3a5f4e33334276987
SRINIVASULU_LAP\vasul <srinivasuppu956@gmail.com>
2026-05-17 02:45:49 +0530

    update default headers for ngrok browser warning

--- src/app-core/services/api-client.js
+++ src/app-core/services/api-client.js
@@ -7,6 +7,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const defaultHeaders = {
 	'Content-Type': 'application/json',
+	'ngrok-skip-browser-warning': 'true',
 };
 
 let refreshPromise = null;
